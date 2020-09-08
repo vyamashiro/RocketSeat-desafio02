827a758745f93b9175de1dd141979880f1ad7185
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2020-09-08 12:41:59 -0300

    permite adicionar um novo repositório

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data)
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Victor Yamashiro'
+    });
+
+    const project = response.data;
+  
+    setProjects([...projects, project]);
   }
 
+
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`)
+
+    const project = response.data;
+  
+    setProjects([...projects, project]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          { projects.map(project => 
+          <li key={project.id}>{project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>Remover</button>
+          </li>
+          )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
